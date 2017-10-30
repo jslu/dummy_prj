6ff9bbe8284722aa70fea48940d7cd8e7e89201b
Ayla-Joe.Lu <joe.lu@aylanetworks.com>
2017-10-30 14:18:19 +0800

    rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DummyPrj::Application.config.secret_token = '8118f159f880ee1bfdc28c140bd82ae3cd48b58943452d96af91079c97eacdb19422179524716983ca7a2defb4916c6ae6bc31e74d45636bdb0d368056459aaa'
